a137bb55c7161a2c240d3d3ce2560efe78145ea2
Said Ferreira <ferreirasaid55@gmail.com>
2023-05-31 22:36:45 -0600

    he creado las clases persona, alumno y profesor

--- src/calificaciones/Alumno.java
+++ src/calificaciones/Alumno.java
@@ -4,55 +4,10 @@
  */
 package calificaciones;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
-class Alumno extends Persona {
-
-    private int numeroLista;
-    private double parcial1;
-    private double parcial2;
-    private double parcial3;
-    private double promedioFinal;
-
-    public Alumno(String nombre, String apellido, int numeroLista) {
-        super(nombre, apellido);
-        this.numeroLista = numeroLista;
-    }
-
-    public int getNumeroLista() {
-        return numeroLista;
-    }
-
-    public double getParcial1() {
-        return parcial1;
-    }
-
-    public double getParcial2() {
-        return parcial2;
-    }
-
-    public double getParcial3() {
-        return parcial3;
-    }
-
-    public double getPromedioFinal() {
-        return promedioFinal;
-    }
-
-    public void setParcial1(double parcial1) {
-        this.parcial1 = parcial1;
-    }
-
-    public void setParcial2(double parcial2) {
-        this.parcial2 = parcial2;
-    }
-
-    public void setParcial3(double parcial3) {
-        this.parcial3 = parcial3;
-    }
-
-    public void setPromedioFinal(double promedioFinal) {
-        this.promedioFinal = promedioFinal;
-    }
+/**
+ *
+ * @author Ssaid
+ */
+public class Alumno {
+    
 }
--- src/calificaciones/Profesor.java
+++ src/calificaciones/Profesor.java
@@ -3,22 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package calificaciones;
-// Clase Profesor que hereda de Persona
 
-class Profesor extends Persona {
-
-    private String materia;
-
-    public Profesor(String nombre, String apellido, String materia) {
-        super(nombre, apellido);
-        this.materia = materia;
-    }
-
-    public String getMateria() {
-        return materia;
-    }
-
-    public void setMateria(String materia) {
-        this.materia = materia;
-    }
+/**
+ *
+ * @author Ssaid
+ */
+public class Profesor {
+    
 }
